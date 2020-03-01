716947858190a61e64b773afc95a75ba67c4bdc3
Liam Childs <liam.h.childs@gmail.com>
2020-03-01 12:46:57 +0100

    update tests

--- dynamic_json/test/test_dynamic_json.py
+++ dynamic_json/test/test_dynamic_json.py
@@ -1,45 +1,156 @@
 import unittest
 
-from dynamic_json import load
+from argparse import ArgumentParser
 from io import StringIO
+from dynamic_json import load
 
 
-class TestDynamicJson(unittest.TestCase):
-    def test_load(self):
-        fileobj = StringIO('''{
-    "project_name": "hello-world",
-    "dirs": {
-        "home": "/home/user",
-        "venv": "{dirs.home}/venvs/{project_name}",
-        "bin": "{dirs.venv}/bin",
-        "data": "{dirs.venv}/data",
-        "errors": "{dirs.data}/errors",
-        "sessions": "{dirs.data}/sessions",
-        "databases": "{dirs.data}/databases"
-    },
-    "exes": {
-        "main": "{dirs.bin}/main",
-        "test": "{dirs.bin}/test"
-    },
-    "bases_per_chromosome": [
-        30432563,
-        19705359,
-        23470805,
-        18585042,
-        26992728,
-        154478,
-        366924
-    ]
-}
-''')
-        cfg = load(fileobj)
-        fileobj.close()
+class TestYamlDict(unittest.TestCase):
+    def test_dict(self):
+        config = '''{
+          "a": 1,
+          "b": 2,
+          "c": "a"
+        }
+        '''
+        res = load(StringIO(config))
+        self.assertDictEqual({'a': 1, 'b': 2, 'c': 'a'}, res)
 
-        self.assertEqual(set(cfg), {'project_name', 'dirs', 'exes', 'bases_per_chromosome'})
-        self.assertEqual(cfg.project_name, 'hello-world')
-        self.assertEqual(cfg.dirs.home, '/home/user')
-        self.assertEqual(cfg.dirs.venv, '/home/user/venvs/hello-world')
+    def test_nested_dict(self):
+        config = '''{
+          "a": 1,
+          "b": {
+            "c": 3,
+            "d": 4,
+            "e": "a"
+          }
+        }
+        '''
+        res = load(StringIO(config))
+        self.assertDictEqual({'a': 1, 'b': {'c': 3, 'd': 4, 'e': 'a'}}, res)
 
-if __name__ == '__main__':
-    import sys
-    sys.exit(unittest.main())
+    def test_deeply_nested_dict(self):
+        config = '''{
+          "a": 1,
+          "b": { 
+            "c": 2,
+            "d": 3,
+            "e": {
+              "f": 4,
+              "g": {
+                "h": 5
+              }
+            }
+          }
+        }
+        '''
+        res = load(StringIO(config))
+        self.assertDictEqual({'a': 1, 'b': {'c': 2, 'd': 3, 'e': {'f': 4, 'g': {'h': 5}}}}, res)
+
+    def test_resolve_simple(self):
+        config = '''{
+          "project_name": "hello-world",
+          "home_dir": "/home/user",
+          "project_dir": "{home_dir}/projects/{project_name}"
+        }
+        '''
+        res = load(StringIO(config))
+
+        self.assertEqual('hello-world', res.project_name)
+        self.assertEqual('/home/user', res.home_dir)
+        self.assertEqual('/home/user/projects/hello-world', res.project_dir)
+
+    def test_resolve_nested(self):
+        config = '''{
+          "project_name": "hello-world",
+          "dirs": { 
+            "home_dir": "/home/user",
+            "project_dir": "{dirs.home_dir}/projects/{project_name}"
+          }
+        }
+        '''
+        res = load(StringIO(config))
+
+        self.assertEqual('hello-world', res.project_name)
+        self.assertEqual('/home/user', res.dirs.home_dir)
+        self.assertEqual('/home/user/projects/hello-world', res.dirs.project_dir)
+
+    def test_resolve_deeply_nested(self):
+        config = '''{
+          "project_name": "hello-world",
+          "dirs": { 
+            "home_dir": "/home/user",
+            "project_dir": "{dirs.home_dir}/projects/{project_name}",
+            "tool1_output_dir": "{dirs.project_dir}/tool1-{parameters.tool1.phase1.subparameter1}-{parameters.tool1.phase1.subparameter2}"
+          },
+          "parameters": {
+            "tool1": {
+              "phase1": {
+                "subparameter1": 0.5,
+                "subparameter2": 1.6666
+              }
+            }
+          }
+        }
+        '''
+        res = load(StringIO(config))
+
+        self.assertEqual('/home/user/projects/hello-world/tool1-0.5-1.6666', res.dirs.tool1_output_dir)
+
+    def test_resolve_simple_update(self):
+        config = '''{
+          "project_name": "hello-world",
+          "dirs": { 
+            "home_dir": "/home/user",
+            "project_dir": "{dirs.home_dir}/projects/{project_name}"
+          }
+        }
+        '''
+        res = load(StringIO(config))
+        self.assertEqual('hello-world', res.project_name)
+        self.assertEqual('/home/user', res.dirs.home_dir)
+        self.assertEqual('/home/user/projects/hello-world', res.dirs.project_dir)
+
+        res.dirs.home_dir = '/winhome/user'
+        self.assertEqual('/winhome/user/projects/hello-world', res.dirs.project_dir)
+
+    def test_resolve_nested_update(self):
+        config = '''{
+          "project_name": "hello-world",
+          "dirs": {
+            "home_dir": "/home/user",
+            "project_dir": "{dirs.home_dir}/projects/{project_name}"
+          }
+        }
+        '''
+        res = load(StringIO(config))
+
+        print(res.items)
+
+        self.assertEqual('hello-world', res.project_name)
+        self.assertEqual('/home/user', res.dirs.home_dir)
+        self.assertEqual('/home/user/projects/hello-world', res.dirs.project_dir)
+
+        res.dirs.database_dir = '{dirs.project_dir}/databases'
+        res.databases = {'customers': '{dirs.database_dir}/customers.sqlite',
+                         'items': '{dirs.database_dir}/items.sqlite'}
+        self.assertEqual('/home/user/projects/hello-world/databases/customers.sqlite', res.databases.customers)
+        self.assertEqual('/home/user/projects/hello-world/databases/items.sqlite', res.databases['items'])
+
+    def test_argparse(self):
+        config = '''{
+          "output_dir": "output-{parameters.parameter1}-{parameters.parameter2}",
+          "parameters": {
+            "parameter1": "a",
+            "parameter2": "b"
+          }
+        }
+        '''
+        res = load(StringIO(config))
+        self.assertEqual('output-a-b', res.output_dir)
+
+        parser = ArgumentParser()
+        parser.add_argument('--parameter1')
+        parser.add_argument('--parameter2')
+        parser.parse_args(('--parameter1', 'c', '--parameter2', 'd'), namespace=res.parameters)
+        self.assertEqual('output-c-d', res.output_dir)
